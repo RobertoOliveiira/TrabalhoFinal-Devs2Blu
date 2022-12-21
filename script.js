79d85fd6c8bca2e4b25c1fe64677f12f8e2f79b8
Ana Lydia Moreira <analydiamoreira965@gmail.com>
2022-12-21 00:59:03 -0300

    Layout Home -Terminado

---
+++ script.js
@@ -0,0 +1,16 @@
+let searchbtn = document.querySelector("#search");
+let searchPizza = document.querySelector(".head .search-bar");
+searchbtn.onclick = () => {
+  searchPizza.classList.toggle("active");
+  menu.classList.remove('active');
+};
+
+let menu = document.querySelector(".head .navbar");
+document.querySelector('#bars').onclick = () =>{
+  menu.classList.toggle('active');
+  searchPizza.classList.remove('active')
+}
+window.onscroll = () => {
+    menu.classList.remove('active');
+    searchPizza.classList.remove('active')
+}
